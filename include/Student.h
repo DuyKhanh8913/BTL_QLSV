398d49f28b815d01645a9dffc4ab7826e1b78226
DuyKhanh8913 <phamduykhanh18032005@gmail.com>
2025-12-01 06:47:24 +0700

    Update Student.h

---
+++ include/Student.h
@@ -0,0 +1,26 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+using namespace std;
+
+class Student {
+private:
+  string id;
+  string studentName;
+  string className;
+
+public:
+  Student() {}
+  Student(string id, string studentName, string className);
+
+  void input();
+  void inputUpdate();
+  void display() const;
+
+  string getId() const;
+  string serialize() const;
+  static Student deserialize(const string &line);
+};
+
+#endif
